2bead3879e82a20b902ac282c7e0e06568c34af2
Ramanna99 <vadde.ramanna99@gmail.com>
2023-05-30 09:02:03 +0530

    REST webservices implementation

--- src/main/java/com/bank/icici/controller/CreateBeneficiaryController.java
+++ src/main/java/com/bank/icici/controller/CreateBeneficiaryController.java
@@ -4,18 +4,69 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bank.icici.bean.Beneficiary;
+
 @RestController
 @RequestMapping(path = "/beneficiaries")
 public class CreateBeneficiaryController {
 
-	public CreateBeneficiaryController() {
-		System.out.println("#################");
-	}
+	@GetMapping(path = "/add", produces = "application/json")
+	public Beneficiary addBeneficiary() {
+		Beneficiary beneficiary = new Beneficiary();
+		beneficiary.setAccountNumber("47548754875");
+		beneficiary.setBankName("HDFC Bank");
+		beneficiary.setIfsccode("HDFC0006475");
+		beneficiary.setName("John");
 
-	@GetMapping(path="/add", produces = "application/json")
-	public String hello() {
-		System.out.println("************");
-		return "Welcome to Spring-boot icici app";
+		return beneficiary;
+
+	}
+	
+	@GetMapping(path = "/update", produces = "application/json")
+	public Beneficiary updateBeneficiary() {
+		Beneficiary beneficiary = new Beneficiary();
+		beneficiary.setAccountNumber("47548754875");
+		beneficiary.setBankName("HDFC Bank");
+		beneficiary.setIfsccode("HDFC0006475");
+		beneficiary.setName("John");
+
+		return beneficiary;
+
+	}
+	
+	@GetMapping(path = "/delete", produces = "application/json")
+	public Beneficiary deleteBeneficiary() {
+		Beneficiary beneficiary = new Beneficiary();
+		beneficiary.setAccountNumber("47548754875");
+		beneficiary.setBankName("HDFC Bank");
+		beneficiary.setIfsccode("HDFC0006475");
+		beneficiary.setName("John");
+
+		return beneficiary;
+
+	}
+	
+	@GetMapping(path = "/get", produces = "application/json")
+	public Beneficiary getBeneficiary() {
+		Beneficiary beneficiary = new Beneficiary();
+		beneficiary.setAccountNumber("47548754875");
+		beneficiary.setBankName("HDFC Bank");
+		beneficiary.setIfsccode("HDFC0006475");
+		beneficiary.setName("John");
+
+		return beneficiary;
+
+	}
+	
+	@GetMapping(path = "/getAll", produces = "application/json")
+	public Beneficiary getAllBeneficiary() {
+		Beneficiary beneficiary = new Beneficiary();
+		beneficiary.setAccountNumber("47548754875");
+		beneficiary.setBankName("HDFC Bank");
+		beneficiary.setIfsccode("HDFC0006475");
+		beneficiary.setName("John");
+
+		return beneficiary;
 
 	}
 
