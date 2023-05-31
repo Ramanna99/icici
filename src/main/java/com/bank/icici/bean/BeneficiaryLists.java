1fafffb524800aaed2e6a3cb748ceb6dd958e4d2
bswarna123 <bommagowniswarna@gmail.com>
2023-05-31 18:58:48 +0530

    new files

---
+++ src/main/java/com/bank/icici/bean/BeneficiaryLists.java
@@ -0,0 +1,18 @@
+package com.bank.icici.bean;
+
+import java.util.List;
+
+public class BeneficiaryLists {
+	
+	private List<Beneficiary>list;
+
+	public List<Beneficiary> getList() {
+		return list;
+	}
+
+	public void setList(List<Beneficiary> list) {
+		this.list = list;
+	}
+	
+
+}
