d43c6d0decb65c0994b5905cc3013cb52841f225
yveeresh967 <yveeresh967@gmail.com>
2023-06-02 07:29:09 +0530

    create the update Api in account controller

--- src/main/java/com/bank/icici/controller/AccountController.java
+++ src/main/java/com/bank/icici/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.bank.icici.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bank.icici.bean.Account;
 
 @RestController
-@RequestMapping(path="/account")
+@RequestMapping(path = "/account")
 public class AccountController {
-	@PostMapping(path="/create",consumes = "application/json")
-	public ResponseEntity<Void> addAccount(@RequestBody Account account)
-	{
+	@PostMapping(path = "/create", consumes = "application/json")
+	public ResponseEntity<Void> addAccount(@RequestBody Account account) {
 		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/update", produces = "application/json")
+	public ResponseEntity<Account> getAccount() {
+		Account account = new Account();
+		account.setId("1001");
+		account.setFirstName("Hari");
+		account.setLastName("Krishna");
+		account.setAccountNumber("564563643534");
+		account.setBranchName("Ballari");
+		account.setBankName("SBI");
+		account.setMobileNumber("9676564654");
+		account.setEmail("hari@gmail.com");
+		account.setIfscCode("SBIN0000779");
+		account.setAmount(25000);
+
+		return new ResponseEntity<Account>(account, HttpStatus.OK);
+
 	}
 
 }
